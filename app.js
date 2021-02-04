6b74a0bc5cd3e83a57f03726766856db0602bb4a
esiomlink <73898643+esiomlink@users.noreply.github.com>
2021-02-04 23:15:37 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const btnMenu = document.querySelector('.logo-menu');
+const menu = document.querySelector('.liste-nav');
+
+btnMenu.addEventListener('click', function(){
+    menu.classList.toggle('active');
+})
+
+const allLinks = document.querySelectorAll('.item-nav');
+
+allLinks.forEach(function(item){
+    item.addEventListener('click', function(){
+
+    
+    menu.classList.toggle('active');
+})
+})
